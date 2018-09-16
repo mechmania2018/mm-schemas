0f18d8fcb7602e959026ad1f862c92ff0b335ca9
Pranay Prakash <pranay.gp@gmail.com>
2018-09-16 15:58:24 -0500

    use cuid instead of uuid

--- models/team.js
+++ models/team.js
@@ -1,13 +1,13 @@
 const { Schema, SchemaTypes } = require('mongoose')
 const { isEmail } = require('validator');
-const uuid = require('uuid/v4')
+const cuid = require('cuid')
 
 const TeamSchema = new Schema({
   name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true, validate: [ isEmail, 'Invalid Email' ] },
   latestScript: { type: SchemaTypes.ObjectId, ref: 'Script' },
   admin: { type: Boolean, default: false },
-  token: { type: String, default: uuid(), index: true }
+  token: { type: String, default: cuid(), index: true }
 })
 
 TeamSchema.methods.canBeAccessedBy = function(team) {
--- scripts/seed-users.js
+++ scripts/seed-users.js
@@ -4,10 +4,16 @@ const { Team } = require('../')(mongoose)
 mongoose.connect(process.env.MONGO_URL)
 mongoose.Promise = Promise
 
-const test = new Team({
+const test1 = new Team({
   name: 'Admin',
   email: 'pranay.gp@gmail.com',
   admin: true
 })
+const test2 = new Team({
+  name: 'User',
+  email: 'arshiam2828@gmail.com',
+  admin: false
+})
 
-test.save()
+test1.save().then(console.log)
+test2.save().then(console.log)
