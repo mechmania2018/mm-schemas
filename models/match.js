1781b28492361c714ec460e9ec34c375ef3913ed
Pranay Prakash <pranay.gp@gmail.com>
2018-09-18 01:15:41 -0500

    don't make the winner unique

--- models/match.js
+++ models/match.js
@@ -2,7 +2,7 @@ const { Schema, SchemaTypes } = require('mongoose')
 
 const MatchSchema = new Schema({
   key: { type: String, required: true, unique: true },
-  winner: { type: Number, required: true, unique: true, default: 3 }
+  winner: { type: Number }
 })
 
 module.exports = m => m.model('Match', MatchSchema)
