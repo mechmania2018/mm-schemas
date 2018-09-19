9b08db287c0ebb8181377d5369336d24324af219
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 18:36:01 -0500

    add length to Match model

--- models/match.js
+++ models/match.js
@@ -2,7 +2,8 @@ const { Schema, SchemaTypes } = require('mongoose')
 
 const MatchSchema = new Schema({
   key: { type: String, required: true, unique: true },
-  winner: { type: Number }
+  winner: { type: Number },
+  length: { type: Number }
 })
 
 module.exports = m => m.model('Match', MatchSchema)
